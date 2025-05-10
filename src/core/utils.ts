6048ea11e557893609f2e94eb79d277d6c81e1fb
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-11 00:13:14 +0800

    fix: support json format

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,6 +1,8 @@
 import path from 'node:path'
 
-export function getModuleFormat(id: string): 'module' | 'commonjs' | undefined {
+export function getModuleFormat(
+  id: string,
+): 'module' | 'commonjs' | 'json' | undefined {
   const ext = path.extname(id)
   switch (ext) {
     case '.mjs':
@@ -9,5 +11,7 @@ export function getModuleFormat(id: string): 'module' | 'commonjs' | undefined {
     case '.cjs':
     case '.cts':
       return 'commonjs'
+    case '.json':
+      return 'json'
   }
 }
