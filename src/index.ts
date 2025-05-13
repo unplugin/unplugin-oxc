ebaa621c8b98d825699fc65ca32a918c677c4370
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-14 00:13:55 +0800

    feat(unloader): named export for json

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,86 @@ import { readFileSync } from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
 import { ResolverFactory } from 'oxc-resolver'
-import { transform } from 'oxc-transform'
+import { transform as oxcTransform } from 'oxc-transform'
 import { createUnplugin, type UnpluginInstance } from 'unplugin'
 import { createFilter } from 'unplugin-utils'
 import { resolveOptions, type Options } from './core/options'
 import { getModuleFormat } from './core/utils'
 import type { RenderedChunk } from 'rollup'
+import type { Plugin as UnloaderPlugin } from 'unloader'
 
 export const Oxc: UnpluginInstance<Options | undefined, false> = createUnplugin(
   (rawOptions = {}, { framework }) => {
     const options = resolveOptions(rawOptions, framework)
     const filter = createFilter(options.include, options.exclude)
+
+    const resolveId =
+      options.resolve !== false
+        ? (id: string, importer?: string, resolveOptions?: any) => {
+            if (
+              !options.resolveNodeModules &&
+              id[0] !== '.' &&
+              !path.isAbsolute(id)
+            )
+              return
+
+            const resolver = new ResolverFactory({
+              extensions: [
+                '.mjs',
+                '.js',
+                '.ts',
+                '.jsx',
+                '.tsx',
+                '.json',
+                '.node',
+              ],
+              conditionNames: resolveOptions?.conditions
+                ? Array.from(resolveOptions.conditions)
+                : ['import', 'require', 'browser', 'node', 'default'],
+              builtinModules: true,
+              ...options.resolve,
+            })
+            const directory = importer ? path.dirname(importer) : process.cwd()
+            const resolved = resolver.sync(directory, id)
+            if (resolved.error?.startsWith('Builtin module')) {
+              return {
+                id,
+                external: true,
+                moduleSideEffects: false,
+              }
+            }
+
+            if (resolved.path) {
+              const format =
+                getModuleFormat(resolved.path) ||
+                resolved.moduleType ||
+                'commonjs'
+              return {
+                id: resolved.path,
+                format,
+              }
+            }
+          }
+        : undefined
+
+    const transform =
+      options.transform !== false
+        ? (code: string, id: string, ...args: any[]) => {
+            const [transformOptions] = args
+            const result = oxcTransform(id, code, {
+              ...options.transform,
+              sourceType: guessSourceType(id, transformOptions?.format),
+              sourcemap: options.sourcemap,
+            })
+
+            if (result.errors.length) {
+              throw new SyntaxError(
+                result.errors.map((error) => error.message).join('\n'),
+              )
+            }
+            return { code: result.code, map: result.map }
+          }
+        : undefined
 
     const renderChunk: any =
       options.minify !== false
@@ -29,101 +98,47 @@ export const Oxc: UnpluginInstance<Options | undefined, false> = createUnplugin(
           }
         : undefined
 
+    const unloader: Partial<UnloaderPlugin> = {
+      options(config) {
+        config.sourcemap ||= options.sourcemap
+      },
+      load(id) {
+        if (id.endsWith('.json')) {
+          let code = readFileSync(id, 'utf8')
+
+          const json = JSON.parse(code)
+          code = `const json = ${code}\nexport default json\n`
+          const i = 0
+          for (const key of Object.keys(json)) {
+            const sanitizedKey = `_${key.replaceAll(/\W/g, '_')}${i}`
+            code +=
+              `\nconst ${sanitizedKey} = json[${JSON.stringify(key)}]\n` +
+              `export { ${sanitizedKey} as ${JSON.stringify(key)} }\n`
+          }
+          return { code, format: 'module' }
+        }
+
+        if (!filter(id)) return
+        const contents = readFileSync(id, 'utf8')
+        return contents
+      },
+    }
+
     return {
       name: 'unplugin-oxc',
       enforce: options.enforce,
 
-      resolveId:
-        options.resolve !== false
-          ? (id, importer, resolveOptions: any) => {
-              if (
-                !options.resolveNodeModules &&
-                id[0] !== '.' &&
-                !path.isAbsolute(id)
-              )
-                return
-
-              const resolver = new ResolverFactory({
-                extensions: [
-                  '.mjs',
-                  '.js',
-                  '.ts',
-                  '.jsx',
-                  '.tsx',
-                  '.json',
-                  '.node',
-                ],
-                conditionNames: resolveOptions?.conditions
-                  ? Array.from(resolveOptions.conditions)
-                  : ['import', 'require', 'browser', 'node', 'default'],
-                builtinModules: true,
-                ...options.resolve,
-              })
-              const directory = importer
-                ? path.dirname(importer)
-                : process.cwd()
-              const resolved = resolver.sync(directory, id)
-              if (resolved.error?.startsWith('Builtin module')) {
-                return {
-                  id,
-                  external: true,
-                  moduleSideEffects: false,
-                }
-              }
-
-              if (resolved.path) {
-                const format =
-                  getModuleFormat(resolved.path) ||
-                  resolved.moduleType ||
-                  'commonjs'
-                return {
-                  id: resolved.path,
-                  format,
-                }
-              }
-            }
-          : undefined,
+      resolveId,
 
       transformInclude(id) {
         return filter(id)
       },
-
-      transform:
-        options.transform !== false
-          ? (code: string, id: string, ...args: any[]) => {
-              const [transformOptions] = args
-              const result = transform(id, code, {
-                ...options.transform,
-                sourceType: guessSourceType(id, transformOptions?.format),
-                sourcemap: options.sourcemap,
-              })
-
-              if (result.errors.length)
-                throw new SyntaxError(
-                  result.errors.map((error) => error.message).join('\n'),
-                )
-              return { code: result.code, map: result.map }
-            }
-          : undefined,
+      transform,
 
       rollup: { renderChunk },
       rolldown: { renderChunk },
       vite: { renderChunk },
-      unloader: {
-        options(config) {
-          config.sourcemap ||= options.sourcemap
-        },
-        load(id) {
-          if (id.endsWith('.json')) {
-            const code = readFileSync(id, 'utf8')
-            return { code, format: 'json' }
-          }
-
-          if (!filter(id)) return
-          const contents = readFileSync(id, 'utf8')
-          return contents
-        },
-      },
+      unloader,
     }
   },
 )
