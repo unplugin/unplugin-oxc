9de023060e21a9ff12a2e61f534b639b9fbf07de
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-20 09:13:17 +0800

    test: skip v20 only

--- tests/unloader.test.ts
+++ tests/unloader.test.ts
@@ -3,19 +3,19 @@ import { exec } from 'tinyexec'
 import { expect, test } from 'vitest'
 
 test('unloader async', async () => {
-  const { exitCode } = await exec('node', [
-    '--import',
-    'unloader/register',
-    'tests/fixtures/unloader.ts',
-  ])
+  const { exitCode } = await exec(
+    'node',
+    ['--import', 'unloader/register', 'tests/fixtures/unloader.ts'],
+    { nodeOptions: { stdio: 'inherit' } },
+  )
   expect(exitCode).toBe(0)
 })
 
-test.skipIf(!process.version.startsWith('v23'))('unloader sync', async () => {
-  const { exitCode } = await exec('node', [
-    '--require',
-    'unloader/register-sync',
-    'tests/fixtures/unloader.cts',
-  ])
+test.skipIf(process.version.startsWith('v20'))('unloader sync', async () => {
+  const { exitCode } = await exec(
+    'node',
+    ['--require', 'unloader/register-sync', 'tests/fixtures/unloader.cts'],
+    { nodeOptions: { stdio: 'inherit' } },
+  )
   expect(exitCode).toBe(0)
 })
