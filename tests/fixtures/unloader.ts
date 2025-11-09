3780ec422277e8d9ac0950bb56122357c84fc24b
Kevin Deng <sxzz@sxzz.moe>
2025-11-09 22:23:19 +0800

    test: import json file

--- tests/fixtures/unloader.ts
+++ tests/fixtures/unloader.ts
@@ -2,8 +2,10 @@
 
 import assert from 'assert'
 import path from 'node:path'
+import pkg from '../../package.json'
 
 assert(path)
+assert(pkg.version)
 
 globalThis.h = (tag) => {
   assert(tag === 'div')
