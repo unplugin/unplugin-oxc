a50eee0559206787e2235ce32f7330b21fd3eb87
Kevin Deng <sxzz@sxzz.moe>
2025-11-13 09:57:30 +0800

    chore: upgrade deps

--- tests/unloader.test.ts
+++ tests/unloader.test.ts
@@ -1,21 +1,23 @@
 import process from 'node:process'
 import { exec } from 'tinyexec'
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
-test('unloader async', async () => {
-  const { exitCode } = await exec(
-    'node',
-    ['--import', 'unloader/register', 'tests/fixtures/unloader.ts'],
-    { nodeOptions: { stdio: 'inherit' } },
-  )
-  expect(exitCode).toBe(0)
-})
+describe.skipIf(process.version.startsWith('v20'))('unloader', () => {
+  test('unloader async', async () => {
+    const { exitCode } = await exec(
+      'node',
+      ['--import', 'unloader/register', 'tests/fixtures/unloader.ts'],
+      { nodeOptions: { stdio: 'inherit' } },
+    )
+    expect(exitCode).toBe(0)
+  })
 
-test.skipIf(process.version.startsWith('v20'))('unloader sync', async () => {
-  const { exitCode } = await exec(
-    'node',
-    ['--require', 'unloader/register-sync', 'tests/fixtures/unloader.cts'],
-    { nodeOptions: { stdio: 'inherit' } },
-  )
-  expect(exitCode).toBe(0)
+  test('unloader sync', async () => {
+    const { exitCode } = await exec(
+      'node',
+      ['--require', 'unloader/register-sync', 'tests/fixtures/unloader.cts'],
+      { nodeOptions: { stdio: 'inherit' } },
+    )
+    expect(exitCode).toBe(0)
+  })
 })
