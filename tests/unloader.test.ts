6c01bbc949624971208948a0108ddb8af6600927
Kevin Deng <sxzz@sxzz.moe>
2026-04-23 10:45:33 +0900

    test: remove unloader async

--- tests/unloader.test.ts
+++ tests/unloader.test.ts
@@ -1,17 +1,7 @@
-import process from 'node:process'
 import { exec } from 'tinyexec'
 import { describe, expect, test } from 'vitest'
 
-describe.skipIf(process.version.startsWith('v20'))('unloader', () => {
-  test('unloader async', async () => {
-    const { exitCode } = await exec(
-      'node',
-      ['--import', 'unloader/register', 'tests/fixtures/unloader.ts'],
-      { nodeOptions: { stdio: 'inherit' } },
-    )
-    expect(exitCode).toBe(0)
-  })
-
+describe('unloader', () => {
   test('unloader sync', async () => {
     const { exitCode } = await exec(
       'node',
